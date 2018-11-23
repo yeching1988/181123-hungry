c117184951dcdac6602637a5ab789d460094a736
ye ching <ye-ching@163.com>
2018-11-23 18:25:34 +0800

    init project

---
+++ src/pages/Msite/Msite.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "Msite"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,6 @@
+//路由器模块
+import Vue from  'vue'
+import VueRouter from  'vue-router'
+
+import Msite from '../pages/Msite/Msite.vue'
+import Msite from '../pages/Msite/Msite.vue'
